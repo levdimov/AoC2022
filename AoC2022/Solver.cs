d93ba80ea9449c9071b5cdb6db5ce774ccc4ffbe
levdimov <dimov@skbkontur.ru>
2022-12-04 14:12:42 +0400

    day3

--- AoC2022/Solver.cs
+++ AoC2022/Solver.cs
@@ -68,6 +68,48 @@ public class Solver
         }
     }
 
+    [Test]
+    public async Task Day3()
+    {
+        var sum = 0;
+        var counter = 0;
+        IEnumerable<char>? items = null;
+        await foreach (var line in ReadInput())
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+
+            items = (items ?? line).Intersect(line);
+            counter += 1;
+
+            if (counter == 3)
+            {
+                counter = 0;
+                sum += Score(items.Single());
+                items = null;
+            }
+        }
+
+        Console.WriteLine(sum);
+
+        int Score(char item)
+        {
+            if (item == 0)
+            {
+                return 0;
+            }
+
+            if (char.IsLower(item))
+            {
+                return item - 'a' + 1;
+            }
+
+            return item - 'A' + 27;
+        }
+    }
+
     private static IAsyncEnumerable<string> ReadInput()
     {
         return File.ReadLinesAsync($"{TestContext.CurrentContext.Test.Name.ToLower(CultureInfo.InvariantCulture)}.txt");
