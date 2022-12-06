302987f1b010923423f8120f43d2b9e40741184b
levdimov <dimov@skbkontur.ru>
2022-12-06 10:20:16 +0400

    day6

---
+++ AoC2022/Day6.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC2022;
+
+public class Day6 : SolverBase
+{
+    protected override string Solve(IEnumerable<string> input)
+    {
+        var buffer = input.Single();
+
+        const int length = 14;
+
+        for (var i = 0; i < buffer.Length - length; i++)
+        {
+            if (buffer[i..(i + length)].Distinct().Count() == length)
+            {
+                return (i + length).ToString();
+            }
+        }
+
+        throw new ArgumentOutOfRangeException();
+    }
+}
